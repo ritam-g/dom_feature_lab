3215b0e05408f597f20d549c82e4dcc1ae0266b3
ritam-g <ritammaty2006@gmail.com>
2025-12-16 22:21:26 +0530

    cross button added

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 let elements = document.querySelectorAll(".element");
 let fullElements = document.querySelectorAll(".full-element");
-let grid = document.querySelector(".all-elements");
+let allElement= document.querySelector(".all-elements");
+let buttons = document.querySelectorAll(".full-element button");
 
+// OPEN FULL VIEW
 elements.forEach(element => {
   element.addEventListener("click", function () {
-    
+    // hide allElement
+    allElement.style.display = "none";
 
-    // hide all full sections first
-    fullElements.forEach(sec => sec.style.display = "none");
+    // hide all full views
+    fullElements.forEach(sec => {
+      sec.style.display = "none";
+    });
 
-    // show selected full section
+    // show selected full view
     fullElements[this.id].style.display = "flex";
+  });
+});
+
+// CLOSE FULL VIEW
+buttons.forEach(btn => {
+  btn.addEventListener("click", function () {
+    // hide all full views
+    fullElements.forEach(sec => {
+      sec.style.display = "none";
+    });
+
+    // show allElement again
+    allElement.style.display = "flex";
   });
 });
